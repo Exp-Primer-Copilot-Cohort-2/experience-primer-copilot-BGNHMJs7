6d44524cd2dbedff56e716f6957ffa1bbd6dc4b9
Feng Linning <165887147+BGNHMJs7@users.noreply.github.com>
2024-08-14 13:01:30 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,8 +1,8 @@
 function skillsMembers() {
-  return [
-    {
-      name: 'Gonzalo Pozzo',
-      avatar: 'https://avatars.githubusercontent.com/u/1394703?v=4',
-      bio: 'I am a full-stack developer and passionate about new technologies. Always looking for new challenges and learning new things.',
-    }]
+  return {
+      restrict: 'E',
+      templateUrl: 'skillsMembers.html',
+      controller: 'SkillsMembersController',
+      controllerAs: 'skillsMembersCtrl'
+  };
 }
