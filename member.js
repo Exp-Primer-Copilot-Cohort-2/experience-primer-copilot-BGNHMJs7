59e55b0887b99537a4de0087e8601c465a745431
Feng Linning <165887147+BGNHMJs7@users.noreply.github.com>
2024-08-14 12:59:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMembers() {
+  return [
+    {
+      name: 'Gonzalo Pozzo',
+      avatar: 'https://avatars.githubusercontent.com/u/1394703?v=4',
+      bio: 'I am a full-stack developer and passionate about new technologies. Always looking for new challenges and learning new things.',
+    }]
+}
